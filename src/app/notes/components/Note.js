5aaaf2a96d9172b44ba6538049f5461b83b76db7
Joshua <joshuafoggnz@gmail.com>
2019-03-01 17:37:39 +1300

    add hover to notes

--- src/app/notes/components/Note.js
+++ src/app/notes/components/Note.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const Note = styled.div`
     ${props => `background: ${props.theme.noteBackgroundColor};`}
     ${props => `border: ${props.theme.noteBorder};`}
     ${props => `color: ${props.theme.pageText};`}
-    ${props => `margin-bottom: ${props.theme.space[3]}px;`}
+    ${props => `padding-bottom: ${props.theme.space[3]}px;`}
     ${props => `padding: ${props.theme.space[3]}px 0;`}
     ${props => `font-size: ${props.theme.fontSizes.gamma}px;`}
     ${props => `border-bottom: ${props.theme.noteBorderStyle};`}
     cursor: pointer;
-`
+    &:hover {
+      ${props => `background: ${props.theme.noteHoverBackground};`} 
+  };
+`;
 
 class StyledNote extends Component {
-    render() {
-        return (
-            <Note>
-                { this.props.note.text }
-            </Note>
-        );
-    }
+  render() {
+    return <Note>{this.props.note.text}</Note>;
+  }
 }
 
 export default StyledNote;
