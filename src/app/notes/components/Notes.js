ced8039c28f1392e5d31f906b0f89f373cbf8470
Joshua <joshuafoggnz@gmail.com>
2019-03-01 17:38:37 +1300

    build NOtes and remove debug logs

--- src/app/notes/components/Notes.js
+++ src/app/notes/components/Notes.js
@@ -24,13 +24,10 @@ const Notes = styled.div`
 class StyledNotes extends Component {
   componentDidMount() {
     this.props.loadNotes();
-    console.log('onchangenewnote', typeof this.props.onChangeNewNote)
   }
 
   handleOnChangeNote = e => {
-      console.log('PROPS: ', this.props)
-    this.props.onChangeNewNote(e.target.value)
-    // this.setState({ newNote: e.target.value });
+    this.props.onChangeNewNote(e.target.value);
   };
   onSubmit = () => {
     this.props.addNote(this.props.newNote);
@@ -47,15 +44,16 @@ class StyledNotes extends Component {
         <NoteWrapper>
           <label>Add another:</label>
           <input
+            value={this.props.newNote}
             id="newNoteInput"
             onChange={this.handleOnChangeNote}
             type="text"
             placeholder="text for new note"
           />
-          {"TODO: DELETE THIS ===== " + JSON.stringify(this.props)}
-          <button onClick={this.onSubmit}>Add</button>
+          <button disabled={!this.props.newNote} onClick={this.onSubmit}>
+            Add
+          </button>
         </NoteWrapper>
-        {JSON.stringify(this.props.notes)}
         {Object.values(this.props.notes).map((note, i) => (
           <Note key={i} note={note} />
         ))}
